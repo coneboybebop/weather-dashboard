b6b920b328d9a20a37468e127b986ed04f21c88d
Connor Allen <m.connor.allen@gmail.com>
2022-05-28 22:39:26 -0400

    first fetch request added

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,51 @@
 
-var cityInputEl = document.querySelector("#city-input")
-var cityFormEl = document.querySelector("#city-form")
+var cityInputEl = document.querySelector("#city-input");
+var cityFormEl = document.querySelector("#city-form");
 
 var searchSubmitHandler = function(event) {
 
-// get text from input
-var city = cityInputEl.value.trim();
+    // prevent page from refreshing
+    event.preventDefault();
 
-var getCoord = function() {
-fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=974d21a8200f2a8724bff9bbf087d1a6")
+    // get text from input
+    var city = cityInputEl.value.trim();
 
-    console.log(getCoord);
+    if (city) {
+        getCoord(city);
+
+        //clear input
+        cityInputEl.textContent = "";
+    } else {
+        alert("please type a city name");
+    };
+
 }
+// api url variable
 
+var getCoord = function(city) {
+var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=974d21a8200f2a8724bff9bbf087d1a6";
+fetch(apiUrl)
+    .then(function(response) {
+        // request was successful
+        if (response.ok) {
+        console.log(response);
+        response.json().then(function(data) {
+            console.log(data);
+            
+        });
+        } else {
+        alert('Error: City Not Found');
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to server");
+    });
+    
+
+};
+
+var getWeather = function(lat, lon) {
+    
 }
 
 // event listener for submitting search
